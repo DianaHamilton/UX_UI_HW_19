5650031146e9e12b88c733dd85c1fbc2d2e26fa9
DianaHamilton <diana@didesignwork.com>
2022-02-14 16:01:03 -0600

    Feb 14

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
 console.log("worked! homework#19");
 
-$( ".search" ).on("click", function() {
-    console.log( "Handler called." );
+function fade() {
+  $('.preloader').fadeOut("slow");
+  }
+  setTimeout(fade, 3000);
+  
+  $(".search").on("click", function(){
+    $(".search").toggleClass("active");
+    if ($(".search").hasClass("active")) {
+      $(".searchBar").css("height", "30vh");
+      $("#searchForm").css("opacity","1");
+      $(".searchIcon").removeClass("fa-search").addClass("fa-times");
+    }
+    else {
+      $(".searchBar").css('height', "0vh");
+      $("#searchForm").css("opacity","0");
+      $(".searchIcon").removeClass("fa-times").addClass("fa-search");
+    }
   });
+  
+  $(".toggle").click(function(){
+    $(this).find(".toggleContainer").toggleClass("active");
+    $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
+  });
+   
